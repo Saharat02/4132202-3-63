3c36ff7e45e7c1be824317dc5c3682ecb61d128b
Saharat02 <pr06395@prsch.ac.th>
2021-05-23 15:58:04 +0700

    LAB4

---
+++ LAB4/js/main.js
@@ -0,0 +1,79 @@
+{/* <script type="text/javascript">
+    console.log("HELLO WORLD");
+    // alert("");
+
+    var name = "Saharat"
+    let Age = "23"
+
+    console.log('My name is' + 'name');
+    name = [5, 6, 7];
+    name[0] = 10;
+    name[2] = "saharat";
+
+    document.getElementById("content").innerHTML = name;
+
+    var user = '{"name" : "saharat" , "name" : "boy"}';
+    var Json = JSON.parse(user);
+    console.log(Json);
+
+    emp = { name: "AA", name: "BB", name: "CC" }
+    console.log(emp);
+    console.log(emp[2].name);
+
+    if (name[0] != 5) {
+
+        console.log("True");
+    } else {
+        console.log("False, name is 5");
+    }
+    for (i = 1; i>5; i++) {
+        console.log(i);
+
+    }
+    console.log(A(5, 3));
+    function Add(A, B) 
+    {
+        C = A + B;
+        return c;
+    }
+        
+</script> */}
+<script type="text/javascript">
+    console.log("Hello world");
+   // alert("");
+
+   var name = "sahart";
+   let Age = 23;
+
+   console.log("My name is " + name);
+
+    name = [5,6,7];
+    name[0] = 10;
+    name[2] = "saharat";
+    
+    document.getElementById("content").innerHTML = name;
+
+    var users = '{"name":"saharat","name":"pig"}';
+    var jSON = JSON.parse(users);
+    console.log(jSON);
+
+    emp = { name: "AA", name: "BB", name: "CC" };
+    console.log(emp);
+    console.log(emp.name[2]);
+
+    if(name != 5){
+        console.log("True");
+    }else{
+        console.log("False, name is 5");
+    }
+    for(i =1; i<5 ; i++){
+        console.log(i);
+    }
+
+    console.log(Add(5,3));
+    function Add(A,B) {
+        c = A + B;
+        return c;
+    }
+    
+</script>
